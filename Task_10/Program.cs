87712f0841deb31d085a3c8e9b99d83776a04eeb
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-14 22:51:58 +0300

    modified:   Task_10/Program.cs new file:   Task_13/Program.cs

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000); // Случайное значение на вход
+Console.Write($"{number} -> ");
+
+// Console.Write("Введите трехзначное число: "); // Ввод значения пользователем
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (number % 100) / 10;
+
+Console.Write(secondNumber);
