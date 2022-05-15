c131572e11661c832708d3e6b9d377b3271b2640
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-15 11:31:09 +0300

    modified:   Task_13/Program.cs new file:   Task_13AlternativeSolution/Program.cs

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,15 +1,12 @@
-﻿
-Console.Write("Введите число: "); // Ввод значения пользователем
+﻿Console.Write("Введите число: "); // Ввод значения пользователем
 string number = Console.ReadLine();
 
 char[] numberArray = number.ToCharArray();
 if (numberArray.Length >= 3)
 {
-    Console.Write("{0} -> ", number);
-    Console.Write(numberArray[2]);
+    Console.Write($"{number} -> {numberArray[2]}");
 }
 else
 {
-    Console.Write("{0} -> ", number);
-    Console.Write("третьей цифры нет");
+    Console.Write($"{number} -> третьей цифры нет");
 }
