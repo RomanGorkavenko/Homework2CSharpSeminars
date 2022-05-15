7bc0958ad6dd8a809d6d8cf2bac4ddc6ab68fadb
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-15 11:47:38 +0300

    new file:   Task_15/Program.cs

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: "); // Ввод значения пользователем
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek == 6 || dayWeek == 7)
+{
+    Console.WriteLine("{0} -> да", dayWeek);
+}
+else
+{
+    Console.WriteLine("{0} -> нет", dayWeek);
+}
+
+// Console.WriteLine(dayWeek == 6 || dayWeek == 7 ? // Альрернативное решение
+//     $"{dayWeek} -> да" :
+//     $"{dayWeek} -> нет");
