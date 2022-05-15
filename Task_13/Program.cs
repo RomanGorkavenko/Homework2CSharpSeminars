58e915fb2c6fd4571b1b20aad917a085a32c38d2
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-15 09:38:42 +0300

    modified:   Task_13/Program.cs

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число: "); // Ввод значения пользователем
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write("{0} -> ", number);
+    Console.Write(numberArray[2]);
+}
+else
+{
+    Console.Write("{0} -> ", number);
+    Console.Write("третьей цифры нет");
+}
