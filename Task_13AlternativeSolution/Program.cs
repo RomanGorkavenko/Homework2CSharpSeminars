73ae7d77961d32fcaa5c0ed23ed1716700644e68
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-16 20:11:34 +0300

    edit to task 13 modified:   Task_13AlternativeSolution/Program.cs

--- Task_13AlternativeSolution/Program.cs
+++ Task_13AlternativeSolution/Program.cs
@@ -1,6 +1,14 @@
 ﻿Console.Write("Введите число: "); // Ввод значения пользователем
 int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(number > 99 ?
-    $"{number} -> {number.ToString()[2]}" :
-    $"{number} -> третьей цифры нет");
+
+int thirdNumber = ThirdNumber(number);
+Console.WriteLine(thirdNumber);
+
+
+static int ThirdNumber(int count)
+{
+    while (count <= -1000 || count >= 1000) count /= 10;
+    int digit = count % 10;
+    return digit;
+}
